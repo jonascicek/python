7b483b286773c90b8a062d577adc7109bdca56ba
jonascicek <jonascicek98@gmail.com>
2024-10-01 13:43:55 +0200

    first commit

---
+++ exercise-01.py
@@ -0,0 +1,11 @@
+# Definieren der Variablen
+firstname = "Jonas"	# String
+lastname = "Cicek" 	# String
+birth_year = "1998" 	# Ganzzahl
+has_license = True	# Boolean (True or False)
+
+# Ausgabe der Variablen
+print("firstname", Name)
+print("lastname", Nachname)
+print("birth_year", Geburtsdatum)
+print("has_license", Besitz_vom_Führerschein)
